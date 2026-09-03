c7676159558507544effac9de0afb33da371781d
shubhkr72 <shubham07262@gmail.com>
2026-09-03 17:59:16 +0530

    health route added

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,6 @@ import authRouter from "./routes/auth.js";
 import profileRouter from "./routes/profile.js";
 import requestRouter from "./routes/request.js";
 import userRouter from "./routes/user.js";
-import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import handleConnection from "./socket/handleConnection.js";
@@ -15,21 +14,9 @@ import chatRouter from "./routes/chat.js";
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  },
-});
+const io = new Server(server);
 
 handleConnection(io);
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 app.use(cookieParser());

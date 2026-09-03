371e3c514449896ac160e321cdc46eb6a3e8f8a8
shubhkr72 <shubham07262@gmail.com>
2026-09-03 18:50:35 +0530

    login update

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -4,24 +4,40 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import { USER_SAFE_DATA } from "../constants.js";
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: isProduction,
+  sameSite: isProduction ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+
 export const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if (!validator.isEmail(email))
+    if (!validator.isEmail(email)) {
       throw new Error("Please enter a valid email address!");
+    }
 
     const user = await User.findOne({ email });
 
-    if (!user) throw new Error("Invalid credentials!");
+    if (!user) {
+      throw new Error("Invalid credentials!");
+    }
 
     const isValidPassword = await user.comparePasswords(password);
 
-    if (!isValidPassword) throw new Error("Invalid credentials!");
+    if (!isValidPassword) {
+      throw new Error("Invalid credentials!");
+    }
 
     const token = await user.getJWT();
 
-    if (!token) throw new Error("Token not found!");
+    if (!token) {
+      throw new Error("Token not found!");
+    }
 
     const safeUserData = USER_SAFE_DATA.reduce((acc, key) => {
       acc[key] = user[key];
@@ -29,10 +45,17 @@ export const loginController = async (req, res) => {
     }, {});
 
     res
-      .cookie("token", token, { expires: new Date(Date.now() + 7 * 360000000) })
-      .json({ message: "Login successfull!", user: safeUserData });
+      .cookie("token", token, cookieOptions)
+      .status(200)
+      .json({
+        message: "Login successful!",
+        user: safeUserData,
+      });
   } catch (error) {
-    res.status(400).send(error?.message || "ERROR: Something went wrong!");
+    console.error("Login error:", error);
+    res
+      .status(400)
+      .send(error?.message || "ERROR: Something went wrong!");
   }
 };
 
@@ -44,12 +67,17 @@ export const registerController = async (req, res) => {
 
     const foundUser = await User.findOne({ email });
 
-    if (foundUser)
-      return res.status(401).send("Account already exists, please login!");
+    if (foundUser) {
+      return res
+        .status(401)
+        .send("Account already exists, please login!");
+    }
 
     const hash = await bcrypt.hash(password, 10);
 
-    if (!hash) throw new Error("Error in password encryption!");
+    if (!hash) {
+      throw new Error("Error in password encryption!");
+    }
 
     const user = new User({
       name,
@@ -62,25 +90,39 @@ export const registerController = async (req, res) => {
 
     const token = await user.getJWT();
 
-    if (!token) throw new Error("Token not found!");
+    if (!token) {
+      throw new Error("Token not found!");
+    }
 
-    // only send user's safe data
     const safeUserData = USER_SAFE_DATA.reduce((acc, key) => {
       acc[key] = user[key];
       return acc;
     }, {});
 
     res
-      .cookie("token", token, { expires: new Date(Date.now() + 7 * 360000000) })
+      .cookie("token", token, cookieOptions)
       .status(201)
-      .json({ message: "Registration successfull!", data: safeUserData });
+      .json({
+        message: "Registration successful!",
+        data: safeUserData,
+      });
   } catch (error) {
-    res.status(400).send(error?.message || "ERROR: Something went wrong!");
+    console.error("Registration error:", error);
+    res
+      .status(400)
+      .send(error?.message || "ERROR: Something went wrong!");
   }
 };
 
 export const logoutController = async (req, res) => {
   res
-    .cookie("token", null, { expires: new Date(Date.now()) })
-    .json({ message: "Logged out successfully!" });
+    .cookie("token", "", {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? "none" : "lax",
+      expires: new Date(0),
+    })
+    .json({
+      message: "Logged out successfully!",
+    });
 };

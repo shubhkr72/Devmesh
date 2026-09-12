016e2291d961dd60c2c0e56238409baf6983dd72
shubhkr72 <shubham07262@gmail.com>
2026-09-12 07:01:22 +0530

    mongo uri update

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-import { MONGO_URI } from "../constants.js";
 
 const connectDb = async () => {
-  await mongoose.connect(MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default connectDb;
--- server/src/constants.js
+++ server/src/constants.js
@@ -1,6 +1,3 @@
-export const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/devtinder";
-
 export const ALLOWED_EDIT_FIELDS = [
   "name",
   "age",
